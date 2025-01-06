c58c50d224fc221e0b9cea297e5348d895162b3f
mcmullarkey <mullarkey.mike@gmail.com>
2025-01-06 16:42:23 -0700

    update to reflect asking questions about dunc'd on episode type

--- data/labeling-app/app.py
+++ data/labeling-app/app.py
@@ -1,65 +1,60 @@
 from pathlib import Path
-
 import pandas as pd
-from shared import INPUTS
 from shiny import reactive
-from shiny.express import input, ui
-from shiny_validate import InputValidator, check
-
+from shiny.express import input, ui, render
+# Set up application directory and load data
 app_dir = Path(__file__).parent
+episodes_csv = app_dir / "podcast_episodes.csv"
+df = pd.read_csv(episodes_csv)
+print(df)
+episode_titles = df["title"].to_list()
+
+# Configure page options and CSS
+ui.page_opts(title="Dunc'd On Episode Type")
 ui.include_css(app_dir / "styles.css")
 
-ui.page_opts(title="Movie survey")
-
 with ui.card():
-    ui.card_header("Demographics")
-    INPUTS["name"]
-    INPUTS["country"]
-    INPUTS["age"]
+    ui.card_header("Episode Info")
+    ui.input_select(
+        "episode",
+        "Episode",
+        choices=episode_titles
+    )
+        
+    @render.data_frame
+    def episodes_df():
+        filtered_df = df.loc[df["title"] == input.episode(),["title", "description"]]
+        return render.DataGrid(filtered_df)
 
+# Episode Type Card
 with ui.card():
-    ui.card_header("Income")
-    INPUTS["income"]
+    ui.card_header("Episode Type")
+    ui.input_radio_buttons(
+        "episode_type",
+        "What episode type is this episode?",
+        choices=["daily_duncs", "hollinger_duncan", "gamer", "big_picture"],
+        selected=[],
+        inline=True,
+    )
+    
+    ui.div(
+        ui.input_action_button("submit", "Submit", class_="btn btn-primary"),
+        class_="d-flex justify-content-end",
+    )
 
-with ui.card():
-    ui.card_header("Ratings")
-    INPUTS["avengers"]
-    INPUTS["spotlight"]
-    INPUTS["the_big_short"]
-
-ui.div(
-    ui.input_action_button("submit", "Submit", class_="btn btn-primary"),
-    class_="d-flex justify-content-end",
-)
-
-# Unfortunate workaround to get InputValidator to work in Express
-input_validator = None
-
-
-@reactive.effect
-def _():
-    # Add validation rules for each input that requires validation
-    global input_validator
-    input_validator = InputValidator()
-    input_validator.add_rule("name", check.required())
-    input_validator.add_rule("country", check.required())
-    input_validator.add_rule("age", check.required())
-    input_validator.add_rule("income", check.required())
-
-
+# Save to CSV effect
 @reactive.effect
 @reactive.event(input.submit)
 def save_to_csv():
-    input_validator.enable()
-    if not input_validator.is_valid():
-        return
-
-    df = pd.DataFrame([{k: input[k]() for k in INPUTS.keys()}])
-
-    responses = app_dir / "responses.csv"
+    fields = {"episode": input.episode(),
+            "episode_type": input.episode_type()
+            }
+    df = pd.DataFrame([fields])
+    responses = app_dir / "episode_types.csv"
+    
     if not responses.exists():
         df.to_csv(responses, mode="a", header=True)
     else:
         df.to_csv(responses, mode="a", header=False)
-
+    
     ui.modal_show(ui.modal("Form submitted, thank you!"))
